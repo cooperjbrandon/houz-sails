a219d68b4ddc9904e5f5f94c751096dba2adb034
Brandon Cooper <cooperjbrandon@gmail.com>
2015-03-29 14:20:27 -0700

    create Listings controller and model

---
+++ api/controllers/ListingsController.js
@@ -0,0 +1,11 @@
+/**
+ * ListingsController
+ *
+ * @description :: Server-side logic for managing listings
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
